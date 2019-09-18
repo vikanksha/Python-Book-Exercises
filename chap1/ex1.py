763c55761c01546fcd7404321ac2dc6780263f17
Your-Full-Name <vikankshakumari09@gmail.com>
2019-09-18 23:07:48 +0530

    Add chap1 example code1

---
+++ chap1/ex1.py
@@ -0,0 +1,9 @@
+choice = eval(input("Enter selection"))
+while choice !="F" and choice != "C":
+    temp = int(input("enter temp to convert"))
+if choice == "F":
+    converted_temp = (temp-32)*5/9
+    print(temp , "degree fahrenheit is equal to" , converted_temp, "degree celcius")
+else:
+    converted_temp = (9/5*temp)+32
+    print(temp , "degree celcius is equal to" , converted_temp, "degree fahrenheit")
