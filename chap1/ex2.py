a0b31c263000e583e43724a9ac01534d8836bd9c
Your-Full-Name <vikankshakumari09@gmail.com>
2019-09-18 23:13:53 +0530

    add chap1 ex2

---
+++ chap1/ex2.py
@@ -0,0 +1,14 @@
+month = int(input("Enter the month(from 1-12) :"))
+if month==2:
+    year = int(input("please enter the year :"))
+    if (year%4==0 and (not(year%100==0)or (year%400==0))):
+        num_of_days = 29
+    else:
+        num_of_days = 28
+elif month in (1,3,5,7,8,10,12):
+        num_of_days = 31
+elif month in (4,6,9,11):
+        num_of_days = 30
+else:
+        print("Invalid Value entered , Please enter valid month")
+print("There are", num_of_days, "Days in the month") 
