02aaaa79985962617298ae20cd8888ec40e53e82
Your-Full-Name <vikankshakumari09@gmail.com>
2019-09-19 14:22:06 +0530

    code

---
+++ chap1/ex2.py
@@ -0,0 +1,2 @@
+# a program that display following power of 2 in same line.
+print(2**1,2**2,2**3,2**4,2**5,2**6,2**7,2**8)
---
+++ chap1/ex3.py
@@ -0,0 +1,3 @@
+integer_value = int(input("what power of two ? :"))
+power = 2**integer_value
+print('two to the power of',integer_value, ':', power)
