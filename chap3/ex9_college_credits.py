f7e64df4f878e9f7a70bba23aea0b0eb84661735
Your-Full-Name <vikankshakumari09@gmail.com>
2019-09-23 16:14:33 +0530

    college credit program

---
+++ chap3/ex9_college_credits.py
@@ -0,0 +1,9 @@
+college_credits = int(input("Enter no of college credits"))
+if college_credits >= 90:
+    print("Senior Status")
+if college_credits >= 60:
+    print("Junior status")
+if college_credits >= 30:
+    print("Sophomore Status")
+else:
+    print("Freshman status")            
