0a4e103af2169162a9f94ca19af6cd9d85baf7c6
Your-Full-Name <vikankshakumari09@gmail.com>
2019-10-06 01:04:33 +0530

    change lower to upper

---
+++ condition_loops program/lower_to_upper.py
@@ -0,0 +1,3 @@
+# Write a Python program that accepts a sequence of lines (blank line to terminate) as input and prints the lines as output (all characters in lower case).
+seq_line = input("Enter sequence of line :")
+seq_line.upper() 
