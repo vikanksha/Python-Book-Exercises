2853a384fca96d1bbb1781d3b7d563a400da8ff7
Your-Full-Name <vikankshakumari09@gmail.com>
2019-10-09 14:46:24 +0530

    display no of days by month

---
+++ condition_loops program/days_no.py
@@ -0,0 +1,10 @@
+print("list of month : January, February, March, April, May, June, July, August, September, October, November, December")
+Month = input("Enter month name :")
+if Month == "Februrary":
+    print("no of days 28/29")
+elif Month in ("January" , "March", "May", "July", "August", "November" , "December"):
+    print("no of days 31")
+elif Month in ("April","June", "September" , "October"):
+    print("no of days 30")
+else:
+    print("wrong month") 
