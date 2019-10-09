0678d9f7f173f8bb6ed070873a6c197acc3a0e8b
Your-Full-Name <vikankshakumari09@gmail.com>
2019-10-09 14:28:49 +0530

    check alphabet is vowel or consonant

---
+++ condition_loops program/check_vowel/conso.py
@@ -0,0 +1,6 @@
+# Write a Python program to check whether an alphabet is a vowel or consonant.
+Alphabet = input("Enter a alphabet :")
+if Alphabet.lower() in "aeiou":
+    print("Vowel")
+else:
+    print("Consonant")    
