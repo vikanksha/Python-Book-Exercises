a788c1a9e96918ec8719a400f6e240fc4d53bfbf
Your-Full-Name <vikankshakumari09@gmail.com>
2019-10-23 14:19:14 +0530

    chinese zodiac sign

--- chap4_list/chinese_zodiac.py
+++ chap4_list/chinese_zodiac.py
@@ -1,5 +1,13 @@
 # chinese Zodiac program
 zodiac_animals = ('rat','ox','tiger', 'rabbit','dragon','snake','horse','goat','monkey','rooster','dog','pig')
-birth_yr = int(input("enter birth yr"))
-cycle_num = (birth_yr-1900)%12
-print(zodiac_animals[cycle_num])
+terminate = False
+while not terminate:
+    
+    birth_yr = int(input("enter birth yr"))
+    cycle_num = (birth_yr-1900)%12
+    print(zodiac_animals[cycle_num])
+    response = input('would you like to enter another year ? (y/n):')
+    while response != 'y' and response != 'n':
+        response = input("please enter 'y' or 'n':")
+    if response == 'n':
+        terminate = True
